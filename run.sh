adbb46676629aedc8df11e4c9269ebd872587203
Riccardo <r.pozza@surrey.ac.uk>
2017-02-17 10:35:12 +0000

    Quick Fix

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -jar leshan-server-demo/target/leshan-server-demo-0.1.11-M15-SNAPSHOT-jar-with-dependencies.jar
+java -jar /home/ubuntu/leshan/leshan-server-demo/target/leshan-server-demo-0.1.11-M15-SNAPSHOT-jar-with-dependencies.jar
 
